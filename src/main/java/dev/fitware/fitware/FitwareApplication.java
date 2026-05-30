5e62054b37e6fe56b6e60cb8604a11c4f941da82
Lucas Meyer <81263064+luccasmeyer@users.noreply.github.com>
2026-05-30 20:54:11 -0300

    Send files

---
+++ src/main/java/dev/fitware/fitware/FitwareApplication.java
@@ -0,0 +1,13 @@
+package dev.fitware.fitware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fitware/fitware/FitwareApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fitware.fitware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
